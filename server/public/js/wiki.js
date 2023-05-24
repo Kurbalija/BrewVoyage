de5e44cbbb9f98580e766b4126d22d54750fe731
Kurbalija <aleksandar.kurbalija@icloud.com>
2023-05-24 11:06:22 +0200

    Wiki update

--- server/public/js/wiki.js
+++ server/public/js/wiki.js
@@ -1,44 +1,53 @@
+// Function to load data from Wikipedia API based on a given topic
 async function loadWikiData(topic) {
-    const url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts|pageimages&exintro&explaintext&piprop=original&titles=${encodeURIComponent(topic)}`;
-  
-    const response = await fetch(url);
-    const data = await response.json();
-  
-    const pages = data.query.pages;
-    const pageId = Object.keys(pages)[0];
-    const content = pages[pageId].extract;
-    const imageUrl = pages[pageId].original ? pages[pageId].original.source : '';
-  
-    // get content div
-    const contentDiv = document.querySelector(".content");
-  
-    // clear content
-    contentDiv.innerHTML = '';
-  
-    // create and append new content
-    const h1 = document.createElement('h1');
-    h1.textContent = topic;
-    contentDiv.appendChild(h1);
-  
-    if (imageUrl) {
-      const img = document.createElement('img');
-      img.src = imageUrl;
-      contentDiv.appendChild(img);
-    }
-  
-    const p = document.createElement('p');
-    p.textContent = content;
-    contentDiv.appendChild(p);
+  // Construct the API URL with the topic parameter
+  const url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts|pageimages&exintro&explaintext&piprop=original&titles=${encodeURIComponent(topic)}`;
+
+  // Fetch data from the API
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // Extract relevant information from the API response
+  const pages = data.query.pages;
+  const pageId = Object.keys(pages)[0];
+  const content = pages[pageId].extract;
+  const imageUrl = pages[pageId].original ? pages[pageId].original.source : '';
+
+  // Get the content div element
+  const contentDiv = document.querySelector(".content");
+
+  // Clear the content of the div
+  contentDiv.innerHTML = '';
+
+  // Create and append new content to the div
+  const h1 = document.createElement('h1');
+  h1.textContent = topic;
+  contentDiv.appendChild(h1);
+
+  if (imageUrl) {
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    contentDiv.appendChild(img);
   }
-  
-  window.onload = function() {
-    const listItems = document.querySelectorAll(".sidenav ul li a");
-  
-    listItems.forEach((item) => {
-      item.addEventListener("click", function (event) {
-        event.preventDefault();
-        loadWikiData(event.target.textContent);
-      });
+
+  const p = document.createElement('p');
+  p.textContent = content;
+  contentDiv.appendChild(p);
+}
+
+// Wait for the window to load before attaching event listeners
+window.onload = function () {
+  // Get all the list items within the sidenav
+  const listItems = document.querySelectorAll(".sidenav ul li a");
+
+  // Attach a click event listener to each list item
+  listItems.forEach((item) => {
+    item.addEventListener("click", function (event) {
+      event.preventDefault();
+      // Get the topic from the data-custom-value attribute
+      const topic = item.getAttribute("data-custom-value");
+      // Call the loadWikiData function with the topic as the argument
+      loadWikiData(topic);
     });
-  };
-  
+  });
+};
